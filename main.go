9b3c4173023cf0c6d62bb383da566ae8b6527934
Toby O'Sullivan <toby@elliptic.co>
2020-05-28 17:14:22 +0100

    Handle a header too

--- main.go
+++ main.go
@@ -9,23 +9,28 @@ import (
 
 // Audit is a simple record that something happened
 type Audit struct {
-	Utc  string `json:"utc" xml:"utc"`
-	Path string `json:"path" xml:"path"`
+	Utc         string `json:"utc" xml:"utc"`
+	Path        string `json:"path" xml:"path"`
+	Fingerprint string `json:"fingerprint" xml:"fingerprint"`
 }
 
 func main() {
 	e := echo.New()
 	e.GET("/one", func(c echo.Context) error {
+		f := c.Request().Header.Get("X-Fingerprint")
 		a := &Audit{
-			Utc:  time.Time.Format(time.Now(), "2006-01-02 15:04:05.000"),
-			Path: "/one",
+			Utc:         time.Time.Format(time.Now(), "2006-01-02 15:04:05.000"),
+			Path:        "/one",
+			Fingerprint: f,
 		}
 		return c.JSON(http.StatusOK, a)
 	})
 	e.GET("/two", func(c echo.Context) error {
+		f := c.Request().Header.Get("X-Fingerprint")
 		a := &Audit{
-			Utc:  time.Time.Format(time.Now(), "2006-01-02 15:04:05.000"),
-			Path: "/two",
+			Utc:         time.Time.Format(time.Now(), "2006-01-02 15:04:05.000"),
+			Path:        "/two",
+			Fingerprint: f,
 		}
 		return c.JSON(http.StatusOK, a)
 	})
